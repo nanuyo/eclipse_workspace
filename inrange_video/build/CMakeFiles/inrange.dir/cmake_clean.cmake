dc78b11a914657dc91bad3f4182716cda7742c7b
hjpark <hjparkg71@gmail.com>
2017-09-18 09:44:30 +0900

    third update

---
+++ inrange_video/build/CMakeFiles/inrange.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inrange.dir/inrange.cpp.o"
+  "inrange.pdb"
+  "inrange"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inrange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
